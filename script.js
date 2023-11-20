b902982e8c5ada1eb2aae406477314e924d63e1c
Rachel Hamby <tnthreat33@gmail.com>
2023-11-19 22:46:51 -0500

    add JS for toggle bio and projects

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-const h2 = document.createElement("h2");
-h2.textContent = "DELETE THIS";
-document.querySelector("body").appendChild(h2);
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleBio = document.getElementById("toggle-bio");
+    const bioSection = document.getElementById("Bio");
+
+    toggleBio.addEventListener("click", function () {
+        bioSection.classList.toggle("expanded");
+        
+        // Toggle visibility of p elements in the #Bio section
+        const bioParagraphs = bioSection.querySelectorAll("p");
+        bioParagraphs.forEach(function (paragraph) {
+            paragraph.classList.toggle("hidden");
+        });
+    });
+
+    const toggleProjects = document.getElementById("toggle-projects");
+    const projectsSection = document.getElementById("project-videos");
+
+    toggleProjects.addEventListener("click", function () {
+        projectsSection.classList.toggle("expanded");
+
+        // Toggle visibility of h5, h6, and iframe elements in the #project-videos section
+        const projectHeadings = projectsSection.querySelectorAll("h5, h6, iframe");
+        projectHeadings.forEach(function (element) {
+            element.classList.toggle("hidden");
+        });
+    });
+});
+
+  
